5bd9c7c400f63a192a1cbf13570a78c978dbab5a
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2023-12-04 15:20:16 +0100

    Added search functionality to client

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import { toJson } from "unsplash-js";
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+import fs from "fs";
 
 dotenv.config();
 
@@ -18,6 +20,7 @@ const unsplash = new Unsplash({
 const app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 
 app.get("/api/photos", (req, res) => {
     unsplash.photos
@@ -27,6 +30,16 @@ app.get("/api/photos", (req, res) => {
         .catch((err) => console.log(err))
 });
 
+app.get("/api/photos/search", (req, res) => {
+    unsplash.search
+    .photos(req.query.keyword, req.query.page, req.query.count)
+    .then(toJson)
+    .then((json) => res.json(json))
+    .catch((err) => console.log(err));
+});
+
 const { PORT = 5000 } = process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`)
+});
